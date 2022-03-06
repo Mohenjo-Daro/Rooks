01e3ee5f92cf2714da9e0b3a5f18a0f4f8ede583
Tyler <tyler_yoo@myabcusd.org>
2022-03-05 18:39:06 -0800

    Changes

--- BFolder/rae.py
+++ BFolder/rae.py
@@ -2,15 +2,51 @@ board = []
 
 print("Welcome to tic tac toe tea teh!")
 print("Same as tic tac toe, but on a 5x5 grid.")
-print("Match 4 first to win!")
+print("Match 5 first to win!")
 
 for row in range(5):
     board.append([" ", " ", " ", " ", " "])   
      
 turn = True
-while True:
+gameOver = False
+
+def check(board):
+    #rows
+    if (board[0][0] == board[0][1] and board[0][0] == board[0][2] and board[0][0] == board[0][3] and board[0][0] == board[0][4]):
+        gameOver = True
+    #if (board[1][0] == board[1][1] and board[1][0] == board[1][2] and board[1][0] == board[1][3] and board[1][0] == board[1][4]):
+    #    print("END")
+    #if (board[2][0] == board[2][1] and board[2][0] == board[2][2] and board[2][0] == board[2][3] and board[2][0] == board[2][4]):
+    #    print("END")
+    #if (board[3][0] == board[3][1] and board[3][0] == board[3][2] and board[3][0] == board[3][3] and board[3][0] == board[3][4]):
+    #    print("END")
+    #if (board[4][0] == board[4][1] and board[4][0] == board[4][2] and board[4][0] == board[4][3] and board[4][0] == board[4][4]):
+    #    print("END")
+
+    #columns
+    #if (board[0][0] == board[1][0] and board[0][0] == board[2][0] and board[0][0] == board[3][0] and board[0][0] == board[4][0]):
+    #    print("END")
+    #if (board[0][1] == board[1][1] and board[0][1] == board[2][1] and board[0][1] == board[3][1] and board[0][1] == board[4][1]):
+    #    print("END")
+    #if (board[0][2] == board[1][2] and board[0][2] == board[2][2] and board[0][2] == board[3][2] and board[0][2] == board[4][2]):
+    #    print("END")
+    #if (board[0][3] == board[1][3] and board[0][3] == board[2][3] and board[0][3] == board[3][3] and board[0][3] == board[4][3]):
+    #    print("END")
+    #if (board[0][4] == board[1][4] and board[0][4] == board[2][4] and board[0][4] == board[3][4] and board[0][4] == board[4][4]):
+    #    print("END")
+
+    #diagonals
+    #if (board[0][0] == board[1][1] and board[0][0] == board[2][2] and board[0][0] == board[3][3] and board[0][0] == board[4][4]):
+    #    print("END")
+    #if (board[0][4] == board[1][3] and board[0][4] == board[2][2] and board[0][4] == board[3][1] and board[0][4] == board[4][0]):
+    #    print("END")
+    
+    
+
+while not gameOver:
     row = input()
     column = input()
+    
     if(int(row) >= 1 and int(row) <= 5):
         if(int(column) >= 1 and int(column) <= 5):
             if (turn):
@@ -40,3 +76,5 @@ while True:
 
     for row in board:
         print(row)
+    
+    check(board)
