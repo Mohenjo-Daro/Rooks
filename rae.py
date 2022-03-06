5e806975d6e6128f4478c8265ac1a72804411160
BRaeDon <50760968+BDan5@users.noreply.github.com>
2022-03-05 18:26:53 -0800

    Add files via upload

---
+++ rae.py
@@ -0,0 +1,17 @@
+board = []
+
+for row in range(3):
+    board.append([" ", " ", " "])
+turn = Truewhile True:
+    row = input()
+    column = input()
+    if(turn):
+        board[int(row) - 1][int(column) -1] = "0"
+    else:
+        board[int(row) - 1][int(column) - 1] = "x"
+
+    turn = not turn
+
+    for row in board:
+        print(row)
+    
