cb43df269ecc4eb4438f32879f4e94f57cf43ca0
BRaeDon <bdanman10@gmail.com>
2022-03-05 16:15:57 -0800

    Actual tic tac toe maybe

--- BFolder/rae.py
+++ BFolder/rae.py
@@ -7,12 +7,19 @@ turn = True
 while True:
     row = input()
     column = input()
-    if(turn):
-        board[int(row) - 1][int(column) -1] = "0"
+    if(int(row) >= 1 and int(row) <= 3):
+        if(int(column) >= 1 and int(column) <= 3):
+            if (turn):
+                board[int(row) - 1][int(column) -1] = "0"
+                turn = not turn
+            else:
+                board[int(row) - 1][int(column) - 1] = "x"
+                turn = not turn
+        
+        else:
+            print("Error, take another turn")
     else:
-        board[int(row) - 1][int(column) - 1] = "x"
-
-    turn = not turn
+        print("Error, take another turn")
 
 
     for row in board:
